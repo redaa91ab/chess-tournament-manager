29f671b02e354cf1674d376e6b67ca2c9f3f66e0
Reda Abdi <redaabdi33@gmail.com>
2025-08-19 17:39:08 +0200

    Menu Manage Players terminé, Menu new tournament en cours de création

--- controller.py
+++ controller.py
@@ -6,6 +6,61 @@ class Controller:
     def __init__(self):
         self.view = View()
 
+    def new_tournament(self):
+
+        #tournament_informations
+        self.view.show_message("\n[bold green]New tournament[/bold green]\n")
+        self.view.show_message("Enter the new tournament details below : ")
+        tournament_name = self.view.get_input("Tournament name : ")
+        place = self.view.get_input("Place : ")
+        number_rounds = self.view.get_input("Number of rounds : ")
+        tournament = Tournament(tournament_name, place, number_rounds)
+
+        #add first 4 players
+        number_players = 0
+        self.view.show_message("\n[bold green]New tournament[/bold green]\n")
+        while number_players < 4 :
+            id = self.view.get_input("Add a player (National Chess ID) :")
+            add_player = tournament.add_players_fromID(id)
+            number_players += 1
+
+            if add_player == None :
+                name = self.view.get_input("Name : ")
+                surname = self.view.get_input("Surname : ")
+                birthdate = self.view.get_input("Birthdate : ")
+                player = Player(id, name, surname, birthdate)
+                player.save_json()
+                tournament.add_players_fromID(id)
+                
+                
+
+
+        
+        #Menu
+        while self.new_tournament:
+
+            self.view.show_message("\n[bold green]New tournament[/bold green]")
+            self.view.show_message("\n1) Add new players")
+            self.view.show_message("2) Start the tournament")
+            self.view.show_message("3)[red] Back[/red]")
+            print(tournament.players)
+            input_live = self.view.get_input("\nChoose an option : ")
+            if input_live == "1" or input_live == "1)":
+                self.view.show_message("\n[bold green]New tournament[/bold green]\n")
+                while number_players >= 4 :
+                
+                    national_id =self.view.get_input("Add a player (National Chess ID) :")
+                    number_players += 1
+
+
+
+                    self.view.get_input("Add a player (National Chess ID) :")
+                    number_players += 1
+                    break
+
+
+
+        
     def ManagePlayers(self):
         self.view.show_message("\n[bold green]Manage Players :[/bold green]\n")
         self.view.show_message("1) See all players")
@@ -20,11 +75,11 @@ class Controller:
 
         elif input_manageplayers == "2" or input_manageplayers == "2)" :
             self.view.show_message("\nEnter the new player details below :")
-            surname = self.view.get_input("Name : ")
-            name = self.view.get_input("Surname : ")
+            id = self.view.get_input("National chess ID : ")
+            name = self.view.get_input("Name : ")
+            surname = self.view.get_input("Surname : ")
             birthdate = self.view.get_input("Birthdate : ")
-            id = self.view.get_input("id : ")
-            player = Player(surname, name, birthdate, id)
+            player = Player(id, name, surname, birthdate )
             player.save_json()
             self.ManagePlayers()
         elif input_manageplayers == "3" or input_manageplayers == "3)" :
@@ -33,7 +88,10 @@ class Controller:
     def run(self):
         self.view.show_menu()
         input_live = self.view.get_input("\nChoose an option : ")
-        if input_live == "3" or input_live == "3)" :
+
+        if input_live == "1" or input_live == "1)":
+            self.new_tournament()
+        elif input_live == "3" or input_live == "3)" :
             self.ManagePlayers()
-        else:
+        else :
             print("echoue")
--- model.py
+++ model.py
@@ -1,14 +1,48 @@
 from tinydb import TinyDB, Query
 db = TinyDB('data.json')
-test = "RR"
+
+
+class Tournament:
+    def __init__(self, tournament_name, place, rounds_number = 3, players = []):
+        self.name = tournament_name
+        self.place = place
+        self.rounds_number = rounds_number
+        self.players = players
+
+    def add_players_fromID(self, id):
+        #self.players.append(player)
+        #print("List of players for this tournament :", self.players)
+        
+
+        # Parcourir les joueurs
+        table = db.table("players")
+        current_player = None
+        for player in table.all():
+            if player.get("National chess ID") == id:
+                current_player = player
+                break  # on arrête dès qu’on trouve
+
+        if current_player:
+            name      = current_player["Name"]
+            surname   = current_player["Surname"]
+            birthdate = current_player["Birthdate"]
+            score     = current_player["Score"]
+            print("We've found your player :",current_player)
+            self.players.append(Player(id, name, surname, birthdate, score))
+            print(self.players)
+            return
+        else:
+            print("We need more information to create a class player")
+            return None
+
 
 class Player:
-    def __init__(self, surname, name, birthdate, id, score = 0.0):
+    def __init__(self, id = None, name = None, surname = None, birthdate = None, score = 0.0):
+        self.id = id #National chess ID        
         self.surname = surname
         self.name = name
         self.birthdate = birthdate
-        self.id = id
-        self.score = score   
+        self.score = score
 
     def add_score(self, points):
         self.score += points
@@ -16,12 +50,15 @@ class Player:
     def save_json(self):
         table = db.table("players")
         table.insert({
+            "National chess ID": self.id,
             "Surname": self.surname,
             "Name": self.name,
             "Birthdate": self.birthdate,
-            "National chess ID": self.id,
             "Score": self.score
         })
+
+
+
     
     def __str__(self):
         return self.surname, self.name, self.birthdate, self.id, self.score
@@ -30,12 +67,18 @@ class Player:
 class Json :
     def __init__(self):
         self.db = db
+
     def show_players(self) :
         table = self.db.table("players")
-        print("\n")
+        print("")
         for item in table.all():
             print(item)
-       
+
+
+
+
+        
+
 
 
 
